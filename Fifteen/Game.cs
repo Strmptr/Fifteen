124ba5c7b23c756cb24ec17a0c2c0c69089b09d0
Andrey Alekseev <alekseev2013andrey@yandex.ru>
2019-06-07 12:33:54 +0700

    Добавьте файлы проекта.

---
+++ Fifteen/Game.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fifteen
+{
+    class Game
+    {
+        int size;//размер
+        int[,] map;//карта
+        int spaceX, spaceY;//пустая ячейка
+        static Random rand = new Random();
+
+
+        public Game (int size)
+        {
+            if (size < 2) size = 2;
+            if (size > 5) size = 5;
+            this.size = size;
+            map = new int[size, size];
+        }
+
+        public void Start()
+        {
+            for (int x = 0; x < size; x++)
+            {
+                for (int y = 0; y < size; y++)
+                {
+                    map[x, y] = CoordsToPosition(x, y) + 1;
+                }
+            }
+            spaceX = size - 1;
+            spaceY = size - 1;
+            map[spaceX, spaceY] = 0;
+        }
+
+        public int GetNumber(int button)
+        {
+            int x, y;
+            PositionToCoords(button, out x, out y);
+            if (x < 0 || x >= size)
+                return 0;
+            if (y< 0 || y >= size)
+                return 0;
+            return map[x, y];
+        }
+
+        private int CoordsToPosition (int x, int y)
+        {
+            if (x < 0)
+                x = 0;
+            if (x > size - 1)
+                x = size - 1;
+            if (y < 0)
+                y = 0;
+            if (y > size - 1)
+                y = size - 1;
+            return y * size + x;
+        }
+
+        private void PositionToCoords (int button, out int x, out int y)
+        {
+            if (button < 0)
+                button = 0;
+            if (button > size * size - 1)
+                button = size * size - 1;
+            x = button % size;
+            y = button / size;
+        }
+
+        public void Shift(int button)//перемещение кнопки
+        {
+            int x, y;
+            PositionToCoords(button, out x, out y);
+            if (Math.Abs(spaceX - x) + Math.Abs(spaceY - y) != 1)//условие перемещения кнопки
+                return;
+            map[spaceX, spaceY] = map[x, y];
+            map[x, y] = 0;
+            spaceX = x;
+            spaceY = y;
+        }
+
+        public void Shuffle()//перемешать
+        {
+            //Shift(rand.Next(0, size * size));
+            int r = rand.Next(0, 4);
+            int x = spaceX;
+            int y = spaceY;
+            switch (r)
+            {
+                case 0: x--; break;
+                case 1: x++; break;
+                case 2: y--; break;
+                case 3: y++; break;
+            }
+            Shift(CoordsToPosition(x, y));
+        }
+
+        public bool isEndGame()
+        {
+            if (!(spaceX == size - 1 && spaceY == size - 1))
+                return false;
+            for (int x = 0; x < size; x++)
+                for (int y = 0; y < size; y++)
+                    if (!(x == size - 1 && y == size - 1))
+                        if (map[x, y] != CoordsToPosition(x, y) + 1)
+                            return false;
+            return true;
+        }
+    }
+}
---
+++ Fifteen/MainWindow.xaml.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Fifteen
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Game game;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            game = new Game(4);
+        }
+
+        private void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            int button = Convert.ToInt32(((Button)sender).Tag);
+            game.Shift(button);
+            Refresh();
+            if (game.isEndGame())
+            {
+                MessageBox.Show("Вы победили!", "Победа");
+                if ((MessageBox.Show("Начать игру заново?", "", MessageBoxButton.YesNo) == MessageBoxResult.Yes))
+                    StartGame();
+                else
+                {
+                    BlockButtons();
+                }
+            }
+        }
+
+        private void BlockButtons()
+        {
+            for (int i = 0; i < 16; i++)
+            {
+                _Button(i).IsEnabled = false;
+            }
+        }
+
+        private void UnblockButtons()
+        {
+            for (int i = 0; i < 16; i++)
+            {
+                _Button(i).IsEnabled = true;
+            }
+        }
+
+        private Button _Button (int button)
+        {
+            switch (button)
+            {
+                case 0: return button0;
+                case 1: return button1;
+                case 2: return button2;
+                case 3: return button3;
+                case 4: return button4;
+                case 5: return button5;
+                case 6: return button6;
+                case 7: return button7;
+                case 8: return button8;
+                case 9: return button9;
+                case 10: return button10;
+                case 11: return button11;
+                case 12: return button12;
+                case 13: return button13;
+                case 14: return button14;
+                case 15: return button15;
+                default: return null;
+            }
+        }
+
+        private void StartGameClick(object sender, RoutedEventArgs e)
+        {
+            StartGame();
+        }
+
+        private void Refresh()
+        {
+            for (int i = 0; i < 16; i++)
+            {
+                _Button(i).Content = game.GetNumber(i).ToString();
+                _Button(i).Visibility = (game.GetNumber(i) > 0) ? Visibility.Visible : Visibility.Hidden;
+            }
+        }
+
+        private void StartGame()
+        {
+            UnblockButtons();
+            game.Start();
+            for (int i = 0; i < 100; i++)
+            {
+                game.Shuffle();
+            }
+            Refresh();
+        }
+
+        private void Main_Loaded(object sender, RoutedEventArgs e)
+        {
+            StartGame();
+        }
+
+
+    }
+}
